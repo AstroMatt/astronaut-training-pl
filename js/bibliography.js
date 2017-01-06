dd57ab053555e672f6b7977b437aad16ee66306a
Matt Harasymczuk <matt@astromatt.space>
2017-01-06 13:10:46 +0100

    Bibliography #time 45m

---
+++ js/bibliography.js
@@ -0,0 +1,11 @@
+var body = document.getElementsByTagName('body')[0];
+
+for (let entry of Bibliography) {
+    let id = Bibliography.indexOf(entry) + 1;
+    let name = new RegExp('\\['+entry.title+'\\]', 'g');
+    let index = '['+id+']';
+    body.innerHTML = body.innerHTML.replace(name, index);
+}
+
+//console.log(content);
+//body.innerHTML = content;
